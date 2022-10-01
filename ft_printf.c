30594a35919afc9af90aa016e71cce5fa9699f0e
nlocusso <nathan.locussol@gmail.com>
2022-10-01 22:23:31 +0200

    printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,33 @@
 /*   By: nlocusso <nlocusso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/01 17:30:38 by nlocusso          #+#    #+#             */
-/*   Updated: 2022/10/01 20:08:00 by nlocusso         ###   ########.fr       */
+/*   Updated: 2022/10/01 22:22:34 by nlocusso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 #include <stdio.h>
-#include "libft.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+
+void	ft_putchar(int c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	write(1, &str[i], 1);
+}
 
 int		nb_fmt(char const *fmt)
 {
@@ -21,9 +41,9 @@ int		nb_fmt(char const *fmt)
 
 	cnt = 0;
 	i = 0;
-	while (str[i] != '\0')
+	while (fmt[i] != '\0')
 	{
-		if (str[i] == '%' && str[i + 1] != '%')
+		if (fmt[i] == '%' && fmt[i + 1] != '%')
 			cnt++;
 		i++;
 	}
@@ -31,35 +51,34 @@ int		nb_fmt(char const *fmt)
 }
 void	fmt_check(const char *fmt, va_list args)
 {
-	if (*fmt == "%c")
-		ft_putchar(args);
-	else if (*fmt == "%s")
-		ft_putstr(args);
-	else if (*fmt == "%p")
+	if (fmt[1] == 'c')
+		ft_putchar(va_arg(args, int));
+	else if (fmt[1] == 's')
+		ft_putstr(va_arg(args, char *));
+	/*else if (fmt[1] == 'p')
 		;
-	else if (*fmt == "%d")
-		ft_putnbr(arsg); # modifier la fonction pour le point
-	else if (*fmt == "%i")
-		ft_putnbr(args);
-	else if (*fmt == "%u")
+	else if (fmt[1] == 'd')
+		ft_putnbr(va_arg(args, char *)); # modifier la fonction pour le point
+	else if (fmt[1] == 'i')
+		ft_putnbr(va_arg(args, char *));
+	else if (fmt[1] == 'u')
 		ft_putnbr(args); # modifier pour les unsign
-	else if (*fmt == "%x")
-		ft_putnbr_base(args); # Full majuscules
-	else if (*fmt == "%X")
-		ft_putnbr_base(args); # Full minuscules
-	else if (*fmt == "%%")
-		write(1, '%', 1);
+	else if (fmt[1] == 'x')
+		ft_putnbr_base(va_arg(args, char *)); # Full majuscules
+	else if (fmt[1] == 'X')
+		ft_putnbr_base(va_arg(args, char *)); # Full minuscules
+	*/
+	else
+		write(1, "Erreur", 6);
 }
 
 char	**tab_for_fmt(const char *fmt, int cnt_fmt)
 {
 	int		i;
-	int		y;
 	int		cnt;
 	char	**tab;
 
 	i = 0;
-	y = 0;
 	cnt = 0;
 	tab = malloc(cnt_fmt * sizeof(char *));
 	while (fmt[i] != '\0')
@@ -67,12 +86,11 @@ char	**tab_for_fmt(const char *fmt, int cnt_fmt)
 		if (fmt[i] == '%' && fmt[i + 1] != '\0')
 		{
 			tab[cnt] = malloc(2 * sizeof(char *));
-			tab[cnt][y] = fmt[i];
-			tab[cnt][y + 1] = fmt[i + 1];
+			tab[cnt][0] = fmt[i];
+			tab[cnt][1] = fmt[i + 1];
 		   	cnt++;
 		}
 		i++;
-		y++;
 	}
 	return (tab);
 }
@@ -80,7 +98,6 @@ char	**tab_for_fmt(const char *fmt, int cnt_fmt)
 int ft_printf(const char *fmt, ...)
 {
 	va_list		args;
-	const char	*value;
 	int			cnt_fmt;
 	int			i;
 	char		**tab_fmt;
@@ -88,11 +105,13 @@ int ft_printf(const char *fmt, ...)
 	i = 0;
 	cnt_fmt = nb_fmt(fmt);
 	tab_fmt = tab_for_fmt(fmt, cnt_fmt);
-	va_start(args, str);
+	va_start(args, fmt);
 	while (i != cnt_fmt)
 	{
-		value = va_arg(args, const char *);
-		check(tab_fmt[i], value);
+		if (tab_fmt[i] == "%%")
+			write(1, "%", 1);
+		else
+			fmt_check(tab_fmt[i], args);
 		i++;
 	}
 	va_end(args);
@@ -101,6 +120,8 @@ int ft_printf(const char *fmt, ...)
 
 int main(int argc, char *argv[])
 {
-	ft_printf("%s", "Salut");
+	ft_printf("%s\n%s", "chat", "chien");
+	printf("\n");
+	printf("%s%s", "chat", "chien");
 	return 0;
 }
